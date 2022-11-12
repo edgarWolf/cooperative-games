23640e01d797d24b55f07787a7c25796d9126e17
edgar <edgar9.wolf.8@gmail.com>
2022-11-12 21:56:13 +0100

    - Add setup.py (provisionally).

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cooperative-games',
+    version='1.0',
+    packages=['games', 'tests', 'indices'],
+    url='',
+    license='',
+    author='Edgar Wolf',
+    author_email='edgar9.wolf.8@gmail.com',
+    description='A python package containing metrics and functionality for cooperative games in context of game theory.'
+)
